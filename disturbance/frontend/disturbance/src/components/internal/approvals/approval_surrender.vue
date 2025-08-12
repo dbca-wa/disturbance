f7bf6168de7566eb99b9179678ac713e425a8f86
Priya Thakur <thakurpriya1990@gmail.com>
2025-08-12 13:06:32 +0800

    add bootstrap and update few approval action fetch post calls

--- disturbance/frontend/disturbance/src/components/internal/approvals/approval_surrender.vue
+++ disturbance/frontend/disturbance/src/components/internal/approvals/approval_surrender.vue
@@ -132,24 +132,26 @@ export default {
             let approval = JSON.parse(JSON.stringify(vm.approval));
             vm.issuingApproval = true;
 
-            vm.$http.post(helpers.add_endpoint_json(api_endpoints.approvals,vm.approval_id+'/approval_surrender'),JSON.stringify(approval),{
-                        emulateJSON:true,
-                    }).then((response)=>{
-                        vm.issuingApproval = false;
-                        vm.close();
-                        swal.fire(
-                             'Surrender',
-                             'An email has been sent to the proponent about surrender of this approval',
-                             'success'
-                        );
-                        vm.$emit('refreshFromResponse',response);
+            fetch(helpers.add_endpoint_json(api_endpoints.approvals,vm.approval_id+'/approval_surrender'),{
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(approval),
+            }).then(async (response)=>{
+                const data = await response.json();
+                vm.issuingApproval = false;
+                vm.close();
+                swal.fire(
+                        'Surrender',
+                        'An email has been sent to the proponent about surrender of this approval',
+                        'success'
+                );
+                vm.$emit('refreshFromResponse',data);
 
-
-                    },(error)=>{
-                        vm.errors = true;
-                        vm.issuingApproval = false;
-                        vm.errorString = helpers.apiVueResourceError(error);
-                    });
+            },(error)=>{
+                vm.errors = true;
+                vm.issuingApproval = false;
+                vm.errorString = helpers.apiVueResourceError(error);
+            });
 
 
         },
